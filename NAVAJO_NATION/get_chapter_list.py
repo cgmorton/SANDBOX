9d40762d560badcdd86567abb517c0ceee5ce036
bdaudert <bdaudert@gmail.com>
2019-01-04 10:35:36 -0800

    NN update

---
+++ NAVAJO_NATION/get_chapter_list.py
@@ -0,0 +1,9 @@
+
+
+
+if __name__ == '__main__':
+    chapters = ['Sanostee', 'BIRDSPRINGS', 'Tsaile/Wheatfields', 'Tsayatoh', 'Fort Defiance', 'Red Mesa','Bodaway/Gap', 'Mexican Springs', 'Round Rock', 'Burnham', 'Cameron', 'Sheepsprings', 'FORT DEFIANCE', 'COYOTE CANYON', 'Crystal', 'Nahodishgish/D Pass','DENNEHOTSO', 'Dilkon', 'Hardrock','Church Rock', 'Nageezi', 'Lupton','Two Grey Hills', 'Lechee', 'Houck', 'Ganado', 'Whitehorse Lake', 'Inscription House', 'Klagetoh', 'CRYSTAL', 'LOWER GREASEWOOD', 'MANUELITO', 'Kayenta','MEXICAN SPRINGS', 'BECENTI','SHEEPSPRINGS', 'SMITH LAKE', 'NAGEEZI','UPPER FRUITLAND','NAVAJO MOUNTAIN', 'Leupp', 'OAK SPRINGS', 'OJO ENCINO', 'OLJATO','WHITEHORSE LAKE', 'ROUND ROCK', 'WHITEHORSE', 'SHIPROCK', 'SMITH LAKE','STANDING ROCK', 'STEAMBOAT', 'Kinlichee', 'TSELANI/COTTONWOOD', 'Two Grey Hills', 'Torreon/Star Lake', 'Lukachukai', 'Tuba City', 'Chichiltah', 'BLACKMESA', 'Sawmill', 'Wide Ruins']
+
+for c in sorted(chapters):
+    print( '\"' + c + '\"' ': ' + '\"' + c + '\"' + ',')
+
