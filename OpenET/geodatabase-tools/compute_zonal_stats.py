fb7c35bd75af992a6ee11d2695e2edab4443d8b7
bdaudert <bdaudert@gmail.com>
2019-03-08 15:38:25 -0800

    optimized zonal stats script

--- OpenET/geodatabase-tools/compute_zonal_stats.py
+++ OpenET/geodatabase-tools/compute_zonal_stats.py
@@ -11,20 +11,12 @@ def set_start_end_date_str(yr_int, m_int):
     Note: ee dates are ot inclusive so we need to set end date
     to the beginning of next months rather than endof this month
     '''
-    if len(str(m_int)) < 2:
+    if len(str(m_int)) == 1:
         m_str = '0' + str(m_int)
     else:
         m_str = str(m_int)
     sd = str(yr_int) + '-' + m_str + '-01'
     ed = str(yr_int) + '-' + m_str + '-' + str(config.statics['mon_len'][m_int - 1])
-    '''
-    if m_int < 10:
-        ed = str(yr_int) + '-' + '0' + str(m_int + 1) + '-01'
-    elif m_int >= 10 and m_int < 12:
-        ed = str(yr_int) + '-' + str(m_int + 1) + '-01'
-    else:
-        ed = str(yr_int + 1) + '-01-01'
-    '''
     return sd, ed
 
 
@@ -58,7 +50,7 @@ def compute_zonal_stats(ee_img, ee_coll_name, feat_coll):
             name = 'NO NAME'
         area = feature.area()
         mean = feature.get('mean')
-        feature = feature.set({'aa_data': [name, area, mean, 4]})
+        feature = feature.set({'aa_data': [name, area, mean]})
         return feature
 
     # Reduce img over the regions of feat_coll
@@ -83,9 +75,11 @@ def compute_zonal_stats(ee_img, ee_coll_name, feat_coll):
         crs='EPSG:4326'
     )
 
-    ee_reducedFeatColl = ee_reducedFeatColl.map(set_aadata)
-    aa_datas = ee_reducedFeatColl.aggregate_array('aa_data').getInfo()
-    return aa_datas
+    # ee_reducedFeatColl = ee_reducedFeatColl.map(set_aadata)
+    # aa_datas = ee_reducedFeatColl.aggregate_array('aa_data').getInfo()
+    # aa_datas = ee_reducedFeatColl.aggregate_array('mean').getInfo()
+    # return aa_datas
+    return ee_reducedFeatColl
 
 
 def arg_parse():
@@ -107,17 +101,12 @@ def arg_parse():
         help='Set the feature collection ID')
 
     parser.add_argument(
-        '-s', '--start', type=str, metavar='YEAR',
+        '-y', '--year', type=str, metavar='YEAR',
         default=(end_dt - dt.timedelta(days=365)).strftime('%Y'),
         help='Start date (format YYYY)')
 
     parser.add_argument(
-        '-e', '--end', type=str, metavar='YEAR',
-        default=end_dt.strftime('%Y'),
-        help='End date (format YYYY)')
-
-    parser.add_argument(
-        '-v', '--variables', nargs='+', default=['et', 'eto', 'etr', 'ndvi'], metavar='VAR',
+        '-v', '--variables', nargs='+', default=['et', 'etr', 'ndvi'], metavar='VAR',
         help='variables')
 
     args = parser.parse_args()
@@ -125,12 +114,14 @@ def arg_parse():
 
 if __name__ == '__main__':
     '''
-    python compute_zonal_stats.py -ai projects/openet/test2/ssebop/monthly_wrs2 -fi users/bdaudert/nasa-roses/usgs_central_valley_mod_base15_ca_poly_170616_wgs84 -v et -s 2017 -e 2017
+    python compute_zonal_stats.py -ai projects/openet/test2/ssebop/monthly_wrs2 -fi users/bdaudert/nasa-roses/usgs_central_valley_mod_base15_ca_poly_170616_wgs84 -y 2017
     '''
+
     start_time = time.time()
     ee.Initialize()
     args = arg_parse()
     coll_name = ''
+    # Get the feature collection name
     feat_colls = config.statics['feature_collections'].keys()
     for feat_coll in feat_colls:
         if config.statics['feature_collections'][feat_coll]['feature_collection_name'] == args.feature_collection_id:
@@ -140,30 +131,44 @@ if __name__ == '__main__':
         raise Exception('Feature Collection not found in statics.feature_collections')
         sys.exit(1)
 
-    year_start_int = int(args.start[0:4])
-    year_end_int = int(args.end[0:4])
-    # Loop over months in each year
-    for yr_int in range(year_start_int, year_end_int + 1):
-        for m_int in range(1, 2):
+    year = int(args.year)
+    ee_feat_coll = ee.FeatureCollection(args.feature_collection_id)
+    ee_img = ee.Image()
+    ee_coll = ee.ImageCollection(args.asset_id)
+    ee_img_list = []
+    for var in args.variables:
+        coll = ee_coll.select(var)
+        for m_int in range(1, 13):
+            m_str = str(m_int)
+            if len(m_str) == 1:
+                m_str = '0' + m_str
             # Set start/end date strings for year and month
-            sd, ed = set_start_end_date_str(yr_int, m_int)
-            # Filter collections by dates
-            ee_coll = ee.ImageCollection(args.asset_id).filterDate(sd, ed)
-            ee_feat_coll = ee.FeatureCollection(args.feature_collection_id)
-            for var in args.variables:
-                # Filetr collection by variable
-                ee_coll = ee_coll.select(var)
-                if var == 'ndvi':
-                    temporal_summary = 'mean'
-                else:
-                    temporal_summary = 'sum'
+            sd, ed = set_start_end_date_str(year, m_int)
+            # Filter collections by dates, and rename the band
+            coll = coll.filterDate(sd, ed).select([var], [var + '_m' + m_str])
 
-                ee_img = compute_temporal_summary(ee_coll, temporal_summary)
-                print(ee_img.propertyNames().getInfo())
-                data = compute_zonal_stats(ee_img, coll_name, ee_feat_coll)
-                data = [[d[0], round(d[1], 4), round(d[2], 4)] for d in data if d[2] is not None]
-                # print(data)
-                # print(sd, ed)
+            # Temporal Summary
+            if var == 'ndvi':
+                temporal_summary = 'mean'
+            else:
+                temporal_summary = 'sum'
+            ee_img = compute_temporal_summary(coll, temporal_summary)
+            ee_img_list.append(ee_img)
+    # Combine images into one multi-band image
+    ee_img = ee.Image.cat(ee_img_list)
+
+    # Zonal Stats
+    reducedFeatColl = compute_zonal_stats(ee_img, coll_name, ee_feat_coll)
+    # data = [[d[0], round(d[1], 4), round(d[2], 4)] for d in data if d[2] is not None]
+    for var in args.variables:
+        for m_int in range(1, 13):
+            m_str = str(m_int)
+            if len(m_str) == 1:
+                m_str = '0' + m_str
+            print('VAR/MONTH ' + var + '/' + m_str)
+            print(reducedFeatColl.aggregate_array(var + '_m' + m_str).getInfo())
+
+
     print("--- %s seconds ---" % (str(time.time() - start_time)))
     print("--- %s minutes ---" % (str((time.time() - start_time) / 60.0)))
     print("--- %s hours ---" % (str((time.time() - start_time) / 3600.0)))
